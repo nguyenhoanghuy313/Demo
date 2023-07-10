f40118e9344f8a9ff10d39d6bda189d412354850
minileisduk <leminhductod@gmail.com>
2023-07-10 22:33:35 +0700

    Revert "Revert "update""
    This reverts commit f2566fcb76eed242e642f250a43b0b977a1a6d6e.

--- Everlane/src/main/java/model/ProductImg.java
+++ Everlane/src/main/java/model/ProductImg.java
@@ -2,17 +2,18 @@ package model;
 
 public class ProductImg {
     int product_Img_ID;
-    String thumbnail, product_Img_1,product_Img_2, product_Img_3;
+    String thumbnail, product_Img_1,product_Img_2, product_Img_3, product_img_name;
 
     public ProductImg() {
     }
 
-    public ProductImg(int product_Img_ID, String thumbnail, String product_Img_1, String product_Img_2, String product_Img_3) {
+    public ProductImg(int product_Img_ID, String thumbnail, String product_Img_1, String product_Img_2, String product_Img_3, String product_img_name) {
         this.product_Img_ID = product_Img_ID;
         this.thumbnail = thumbnail;
         this.product_Img_1 = product_Img_1;
         this.product_Img_2 = product_Img_2;
         this.product_Img_3 = product_Img_3;
+        this.product_img_name = product_img_name;
     }
 
     public int getProduct_Img_ID() {
@@ -53,5 +54,13 @@ public class ProductImg {
 
     public void setProduct_Img_3(String product_Img_3) {
         this.product_Img_3 = product_Img_3;
+    }
+
+    public String getProduct_img_name() {
+        return product_img_name;
+    }
+
+    public void setProduct_img_name(String product_img_name) {
+        this.product_img_name = product_img_name;
     }
 }
