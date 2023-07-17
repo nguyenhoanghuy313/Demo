5b9e086972b20e4b83e7a695977013a35fe2a9ff
Whatever0313 <105837211+nguyenhoanghuy313@users.noreply.github.com>
2023-07-17 23:00:52 +0700

    Update CategoryDAO.java

--- Everlane/src/main/java/model/CategoryDAO.java
+++ Everlane/src/main/java/model/CategoryDAO.java
@@ -5,7 +5,54 @@ import java.util.List;
 
 public class CategoryDAO extends myDAO {
 
+    public List<Category> getAllCategory() {
+        List<Category> t = new ArrayList<>();
+        xSql = "select * from Category";
+        try {
+            ps = con.prepareStatement(xSql);
+            rs = ps.executeQuery();
+            int xId;
+            String xName, xImg;
+            Category c;
+            while (rs.next()) {
+                xId = rs.getInt("CategoryID");
+                xName = rs.getString("CategoryName");
+                xImg = rs.getString("Category_img");
+                c = new Category(xId, xName, xImg);
+                t.add(c);
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (t);
+    }
 
+    public Category getCategory(String xId) {
+        int i = Integer.parseInt(xId);
+        xSql = "select * from category where CategoryID = ?";
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setInt(1, i);
+            rs = ps.executeQuery();
+            String xcateName;
+            String xcateImg;
+            Category x;
+            while (rs.next()) {
+                i = rs.getInt("CategoryID");
+                xcateName = rs.getString("CategoryName");
+                xcateImg = rs.getString("Category_img");
+                x = new Category(i, xcateName, xcateImg);
+                return x;
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     public void updateCategory(String xcateImg, int xcateId) {
         try {
