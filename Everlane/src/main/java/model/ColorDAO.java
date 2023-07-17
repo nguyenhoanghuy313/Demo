40d5104e92d9ad6ae2c26c4412bf8876a36e1537
Whatever0313 <105837211+nguyenhoanghuy313@users.noreply.github.com>
2023-07-17 23:03:54 +0700

    update color and Product DAO

--- Everlane/src/main/java/model/ColorDAO.java
+++ Everlane/src/main/java/model/ColorDAO.java
@@ -5,6 +5,28 @@ import java.util.List;
 
 public class ColorDAO extends myDAO{
 
+    public List<Color> getAllColors() {
+        List<Color> t = new ArrayList<>();
+        xSql = "select * from color;";
+        try {
+            ps = con.prepareStatement(xSql);
+            rs = ps.executeQuery();
+            int xColor_ID;
+            String xColor_Name;
+            Color x;
+            while (rs.next()) {
+                xColor_ID = rs.getInt("color_ID");
+                xColor_Name = rs.getString("color_Name");
+                x = new Color(xColor_ID, xColor_Name);
+                t.add(x);
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (t);
+    }
 
 
 
